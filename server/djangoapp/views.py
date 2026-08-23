8bf7b9e044d023adc261086d7e84d12fdcbe1c3f
jjfy999 <justinyehjiafeng@gmail.com>
2026-08-23 09:58:34 -0400

    liniting stuff

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,6 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
+# from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
@@ -40,6 +40,7 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
 
@@ -47,16 +48,17 @@ def logout_request(request):
     data = {"userName": ""}
     return JsonResponse(data)
 
+
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    
+
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
     first_name = data['firstName']
     last_name = data['lastName']
-    email = data ['email']
+    email = data['email']
 
     username_exist = False
     try:
@@ -64,14 +66,14 @@ def registration(request):
         username_exist = True
     except User.DoesNotExist:
         logger.debug(f"{username} is new user")
-    
+
     if not username_exist:
         user = User.objects.create_user(
-            username = username,
-            first_name = first_name,
-            last_name = last_name,
-            password = password,
-            email = email
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email
         )
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -81,15 +83,14 @@ def registration(request):
         return JsonResponse(data)
 
 
-
-#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+
+    if (state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
