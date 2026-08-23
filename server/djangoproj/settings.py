a86bd09d78312eae6fea6acbf02cb75e7e9815c4
jjfy999 <justinyehjiafeng@gmail.com>
2026-08-23 10:10:29 -0400

    liniting stuff

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     'justinyehjia-8000.theiadockernext-1-labs-prod-"
-    "theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    "theiak8s-4-tor01.proxy.cognitiveclass.ai']"
 CSRF_TRUSTED_ORIGINS = [
     'justinyehjia-8000.theiadockernext-1-labs-prod-"
     "theiak8s-4-tor01.proxy.cognitiveclass.ai']
