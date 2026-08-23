3bcb0bef0c510ad5168579fea0dcfc7dcd9339d2
jjfy999 <justinyehjiafeng@gmail.com>
2026-08-23 10:15:32 -0400

    liniting stuff

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = [
     "justinyehjia-8000.theiadockernext-1-labs-prod-"
     "theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 CSRF_TRUSTED_ORIGINS = [
-    "justinyehjia-8000.theiadockernext-1-labs-prod-"
-    "theiak8s-4-tor01.proxy.cognitiveclass.ai]
+    "https://justinyehjia-8000.theiadockernext-1-labs-prod-"
+    "theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
